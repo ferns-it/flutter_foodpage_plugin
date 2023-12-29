0ea8946d71e6fb77ff6a65ce7bf790d85865afb4
Sankaranarayanan-P <sankaranarayananp2021@gmail.com>
2023-12-29 15:13:12 +0530

    base client upgraded

--- lib/table_reservation/base_client.dart
+++ lib/table_reservation/base_client.dart
@@ -1,108 +1,197 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_foodpage_plugin/table_reservation/constants/api_endpoints.dart';
+import 'package:flutter_foodpage_plugin/table_reservation/services/shared_preference/auth_preference.dart';
 
-import '../table_reservation/exceptions/app_exceptions.dart';
+import 'services/app_exception/app_exception.dart';
 
 class BaseClient {
-  static const _connectionTimeOut = Duration(milliseconds: 30000);
-  static const _recieveTimeOut = Duration(milliseconds: 30000);
+  static const int _timeLimit = 120;
   static ResponseType get responeType => ResponseType.json;
+  static String get contentType => "application/json";
 
   static BaseOptions get _baseOptions => BaseOptions(
-        connectTimeout: _connectionTimeOut,
-        receiveTimeout: _recieveTimeOut,
-        baseUrl: ApiEndpoints.baseUrl,
-        contentType: "application/json",
-        responseType: responeType,
-      );
+      connectTimeout: const Duration(seconds: _timeLimit),
+      receiveTimeout: const Duration(seconds: _timeLimit),
+      baseUrl: ApiEndpoints.baseUrl,
+      contentType: contentType,
+      responseType: responeType,
+      headers: {"SOURCE": "OWNER", "Language": "en"});
 
   static Dio get dio => Dio(_baseOptions)
     ..interceptors.add(
       InterceptorsWrapper(
+        onRequest: requestInterceptorHandler,
         onResponse: responseInterceptorHandler,
-        // onError: errorResponseHandler,
+        onError: errorResponseHandler,
       ),
     );
+  // ..interceptors.add(LogInterceptor(
+  //   error: true,
+  //   request: true,
+  //   requestHeader: true,
+  //   requestBody: true,
+  //   responseBody: true,
+  // ));
+
+  static void requestInterceptorHandler(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final needAuth = (options.headers["needToken"] as bool);
+    if (!needAuth) return handler.next(options);
+    final authenticationKey = await AuthPreference().readAuthKeyData();
+    if (authenticationKey == null) {
+      final error = UnauthorizedAccessException();
+      return handler.reject(
+        DioException(
+          requestOptions: options,
+          message: error.message,
+          type: DioExceptionType.unknown,
+          error: error,
+        ),
+      );
+    }
+    options.headers.remove("needToken");
+    options.headers.addAll({"x-shopToken": authenticationKey});
+    handler.next(options);
+  }
 
   static void responseInterceptorHandler(
     Response<dynamic> response,
     ResponseInterceptorHandler handler,
   ) {
     final data = response.data;
-    final res = jsonDecode(data);
-    response.data = jsonEncode(res['data']);
-    return handler.next(response);
+
+    final dataKey = response.requestOptions.headers["dataKey"] as String?;
+
+    // Check if the data is null
+    if (data != null) {
+      try {
+        final res = jsonDecode(data);
+
+        // Check if 'success' or 'status' is present and not null
+        // final success = res['success'] ?? res["status"] ?? false;
+
+        // Check if 'data' is present and not null
+        final responseData = res[dataKey ?? 'data'];
+
+        if (responseData == null) {
+          final exception = ReponseSyntaxException();
+          return handler.reject(DioException(
+            requestOptions: response.requestOptions,
+            error: exception,
+            message: exception.message,
+            type: DioExceptionType.badResponse,
+          ));
+        }
+        response.data = jsonEncode(responseData);
+
+        return handler.next(response);
+      } catch (e) {
+        final exception = FormatErrorException();
+        return handler.reject(DioException(
+          requestOptions: response.requestOptions,
+          error: exception,
+          message: exception.message,
+          type: DioExceptionType.badResponse,
+        ));
+      }
+    }
+
+    // If any of the checks fail, consider it a bad response
+    final exception = InternalServerErrorException();
+    return handler.reject(DioException(
+      requestOptions: response.requestOptions,
+      error: exception,
+      message: exception.message,
+      type: DioExceptionType.badResponse,
+    ));
   }
 
   static void errorResponseHandler(
-    DioException error,
+    DioException dioError,
     ErrorInterceptorHandler handler,
-  ) {
-    final response = error.response?.data;
-    final status = error.response?.statusCode ?? 500;
-    final errorObject = response is String ? jsonDecode(response) : null;
+  ) async {
+    final type = dioError.type;
+    final res = dioError.response;
+    final data = res?.data;
 
     String? message;
 
-    if (errorObject is Map<String, dynamic>) {
-      message =
-          errorObject["messages"]["error"] ?? errorObject['No order found'];
+    if (data is String) {
+      try {
+        final jsonData = json.decode(data) as Map<String, dynamic>?;
+        message =
+            jsonData?['error']?['message'] ?? jsonData?['data']?['message'];
+      } catch (_) {
+        debugPrint('Not JSON Decodable: $data');
+      }
     }
 
-    if (error.error is SocketException) {
-      throw ConnectionLostException();
+    AppExceptions exception;
+
+    switch (type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+        exception = ConnectionSlowException();
+        break;
+      case DioExceptionType.receiveTimeout:
+        exception = TimeoutException();
+        break;
+      case DioExceptionType.badCertificate:
+      case DioExceptionType.badResponse:
+        exception = InternalServerErrorException(message: message);
+        break;
+      case DioExceptionType.cancel:
+        exception = UserCancelException();
+        break;
+      case DioExceptionType.connectionError:
+        exception = ConnectionLostException();
+        break;
+      case DioExceptionType.unknown:
+        exception = InternalServerErrorException(message: message);
+        break;
     }
 
-    if (error.type == DioExceptionType.connectionTimeout ||
-        error.type == DioExceptionType.receiveTimeout ||
-        error.type == DioExceptionType.sendTimeout) {
-      throw TimeOutException();
-    }
-
-    switch (status) {
-      //unauthorized
-      case 401:
-        throw TokenExpiredException();
-      //forbidden
-      case 403:
-        throw UnAuthorizedException(
-          message: message ??
-              "Sorry, you are not authorized to access this resource.",
-        );
-      //not found
-      case 404:
-        throw NotFoundException(
-          message: message ?? "Oops, we couldn't find your request.",
-        );
-      default:
-        throw InternalServerException(
-          message:
-              message ?? "Oops, something went wrong. Please try again later.",
-        );
-    }
+    return handler.reject(DioException(
+      requestOptions: dioError.requestOptions,
+      error: exception,
+      type: type,
+      message: exception.message,
+    ));
   }
 
   //GET
   static Future<String?> get({
     bool needAuth = false,
     String? baseUrl,
+    String? dataKey,
     required String api,
     String params = '',
+    Map<String, dynamic>? additionalHeaders,
     Map<String, dynamic>? queryParameters,
+    String contentType = Headers.jsonContentType,
   }) async {
     final url = baseUrl != null ? baseUrl + api + params : api + params;
+    var headers = <String, dynamic>{
+      'content-type': contentType,
+      'dataKey': dataKey,
+    };
 
-    const authKey = '5521bacd985f98bbcb30c9e0f1a242ae';
+    if (additionalHeaders != null) {
+      headers.addAll(additionalHeaders);
+    }
 
     final response = await dio.get<String>(
       url,
       queryParameters: queryParameters,
-      options: Options(headers: {"x-shopToken": authKey}),
+      options: Options(headers: headers),
     );
+
     return response.data;
   }
 
@@ -110,62 +199,111 @@ class BaseClient {
   static Future<String?> post({
     bool needAuth = false,
     String? baseUrl,
+    String? dataKey,
     required String api,
-    required Object data,
+    Object? data,
+    String params = '',
+    Map<String, dynamic>? additionalHeaders,
     Map<String, dynamic>? queryParameters,
+    String contentType = Headers.jsonContentType,
   }) async {
-    const authKey = '5521bacd985f98bbcb30c9e0f1a242ae';
+    final url = baseUrl != null ? baseUrl + api + params : api + params;
+    var headers = <String, dynamic>{
+      'content-type': contentType,
+      'dataKey': dataKey,
+    };
+
+    if (additionalHeaders != null) {
+      headers.addAll(additionalHeaders);
+    }
 
     final response = await dio.post<String>(
-      api,
+      url,
       data: data,
       queryParameters: queryParameters,
-      options: Options(headers: {"x-shopToken": authKey}),
+      options: Options(headers: headers),
     );
+
     return response.data;
   }
 
   //PUT
   static Future<String?> put({
+    bool needAuth = false,
+    String? baseUrl,
+    String? dataKey,
+    required String api,
+    Object? data,
+    String params = '',
+    Map<String, dynamic>? additionalHeaders,
+    Map<String, dynamic>? queryParameters,
+    String contentType = Headers.jsonContentType,
+  }) async {
+    final url = baseUrl != null ? baseUrl + api + params : api + params;
+    var headers = <String, dynamic>{
+      'content-type': contentType,
+      'dataKey': dataKey,
+    };
+
+    if (additionalHeaders != null) {
+      headers.addAll(additionalHeaders);
+    }
+
+    final response = await dio.put<String>(
+      url,
+      data: data,
+      queryParameters: queryParameters,
+      options: Options(headers: headers),
+    );
+
+    return response.data;
+  }
+
+  //PATCH
+  static Future<String?> patch({
     bool needAuthentication = false,
     String? baseUrl,
     required String api,
     String params = '',
     Object? data,
     Map<String, dynamic>? queryParameters,
-    Map<String, dynamic>? additionalHeaders,
   }) async {
-    const authKey = '5521bacd985f98bbcb30c9e0f1a242ae';
-
-    // Prepare headers with the additional headers if not null
-    Map<String, dynamic> headers = {"x-shopToken": authKey};
-    if (additionalHeaders != null) {
-      headers.addAll(additionalHeaders);
-    }
-
-    final response = await dio.put<String>(
-      api + params,
-      data: data,
-      queryParameters: queryParameters,
-      options: Options(headers: headers), // Use the prepared headers
-    );
-
+    final response = await dio.patch<String>(api + params,
+        data: data,
+        queryParameters: queryParameters,
+        options: Options(headers: {'needToken': needAuthentication}));
     return response.data;
   }
 
   //DELETE
   static Future<String?> delete({
-    bool needAuthentication = false,
+    bool needAuth = false,
     String? baseUrl,
+    String? dataKey,
     required String api,
     Object? data,
+    String params = '',
+    Map<String, dynamic>? additionalHeaders,
     Map<String, dynamic>? queryParameters,
+    String contentType = Headers.jsonContentType,
   }) async {
+    final url = baseUrl != null ? baseUrl + api + params : api + params;
+    var headers = <String, dynamic>{
+      'content-type': contentType,
+      'dataKey': dataKey,
+    };
+
+    if (additionalHeaders != null) {
+      headers.addAll(additionalHeaders);
+    }
+
     final response = await dio.delete<String>(
-      api,
+      url,
       data: data,
       queryParameters: queryParameters,
+      options: Options(headers: headers),
     );
+
     return response.data;
   }
 }
