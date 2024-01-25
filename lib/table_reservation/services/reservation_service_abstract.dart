b6df2a099e8b1c2d498d0f4537b3d727be28c786
rahulreghupdm <rahulreghupdm@gmail.com>
2024-01-25 10:01:50 +0530

    search query added

--- lib/table_reservation/services/reservation_service_abstract.dart
+++ lib/table_reservation/services/reservation_service_abstract.dart
@@ -1,20 +1,17 @@
 import 'package:flutter_foodpage_plugin/flutter_foodpage_plugin.dart';
-import 'package:flutter_foodpage_plugin/table_reservation/models/reservation/new_reservation_model.dart';
 import 'package:flutter_foodpage_plugin/table_reservation/services/table_reservation_service.dart';
 
-import '../constants/enums.dart';
 import '../models/history/history_request_collection_model.dart';
 import '../models/send_message/send_message_model.dart';
-import '../models/upcoming_request/upcoming_request_collection_model.dart';
 
 abstract class ReservationService {
   static final ReservationService _instance = TableReservationService();
   static ReservationService get instance => _instance;
   Future<EnquirieModel?> newReservation(NewReservationModel reservation);
-  Future<NewRequestCollectionModel?> getNewRequests();
-  Future<UpcomingRequestCollection?> getUpcomingList();
+  Future<NewRequestCollectionModel?> getNewRequests({String? searchQuery});
+  Future<UpcomingRequestCollection?> getUpcomingList({String? searchQuery});
   Future<ReservationDetailsModel?> getReservationDetails(String reservationID);
-  Future<ReservationHistoryRequestCollectionModel?> getReservationHistory();
+  Future<ReservationHistoryRequestCollectionModel?> getReservationHistory({String? searchQuery});
   Future<ResponseResult> approveOrCancelReservation(
     ApproveOrCancelRequestModel payload,
   );
