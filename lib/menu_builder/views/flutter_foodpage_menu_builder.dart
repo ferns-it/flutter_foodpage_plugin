a99dc3de23c995c5528e616577b30db6916d07df
Sankaranarayanan-P <sankaranarayananp2021@gmail.com>
2024-06-10 14:10:48 +0530

    FoodDetailsSideSheetWidget added

--- lib/menu_builder/views/flutter_foodpage_menu_builder.dart
+++ lib/menu_builder/views/flutter_foodpage_menu_builder.dart
@@ -4,6 +4,7 @@ import 'package:flutter_foodpage_plugin/menu_builder/controllers/core/providers_
 import 'package:flutter_foodpage_plugin/menu_builder/views/common/root/base_root_widget.dart';
 import 'package:flutter_foodpage_plugin/menu_builder/views/dashboard/dashboard_screen.dart';
 import 'package:flutter_foodpage_plugin/menu_builder/views/food_menu/food_menu_screen.dart';
+import 'package:flutter_foodpage_plugin/menu_builder/views/food_menu/widgets/food_details_side_sheet_widget.dart';
 import 'package:provider/provider.dart';
 
 class FlutterFoodpageMenuBuilder extends StatelessWidget {
@@ -21,7 +22,10 @@ class FlutterFoodpageMenuBuilder extends StatelessWidget {
       child: Builder(builder: (context) {
         final pageNavigationController =
             context.watch<PageNavigationController>();
+        final currentPageIndex = pageNavigationController.currentPageIndex;
         return BaseRootWidget(
+          endDrawer:
+              currentPageIndex == 1 ? const FoodDetailsSideSheetWidget() : null,
           child: Expanded(
             child: screens[
                 pageNavigationController.currentPageIndex % screens.length],
