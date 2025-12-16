573c17c48abb63ba2da720894b3401512dcbea2a
hashcode404 <pranavps962@gmail.com>
2025-12-16 15:09:27 +0530

    Table reservation endpoint spelling mistake

--- lib/table_reservation/constants/api_endpoints.dart
+++ lib/table_reservation/constants/api_endpoints.dart
@@ -7,7 +7,7 @@ class ApiEndpoints {
   static const getNewRequests = '/getnewrequests';
   static const getUpcomingRequests = '/getupcominglist';
   static const getdata = '/getdata';
-  static const updateRequest = '/enruiryupdate/';
+  static const updateRequest = '/enquiryupdate/';
   static const sendCustomerMessage = '/sendcustomermessage';
   static const gethistory = '/gethistory';
   static const captureamount = '/captureamount/';
